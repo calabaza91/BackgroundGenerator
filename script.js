b94ec2d220323ae157c05b30f0ceac46b962acaa
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-21 12:53:14 -0600

    Update js file
    Sets initial color inputs for gradient and css text content on page load

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+//Runs initial gradient and css textContent when page loads
+window.onload = setGradient;
+
 function setGradient(){
   body.style.background = "linear-gradient(to right, "
     + color1.value + ", " + color2.value + ")";
