9d007b0b473c2973d96dc37d4a9cd439d7220472
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-21 14:09:52 -0600

    Add a button to randomly set gradients
    Html: add button element
    Css: style button
    Js: add random color getter and setter functions to change color input values

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("random");
 
 //Runs initial gradient and css textContent when page loads
 window.onload = setGradient;
@@ -14,6 +15,25 @@ function setGradient(){
   css.textContent = body.style.background + ";";
 }
 
+//Get random hex color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+//Set Random color
+function setRandomColor(){
+  color1.value = getRandomColor();
+  color2.value = getRandomColor();
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandomColor);
